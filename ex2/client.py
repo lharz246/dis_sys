987bcee958a768fb1a0ecd10f95ce3620c07ae7c
lharz <lharz@techfak.uni-bielefeld.de>
2024-09-28 17:24:30 +0200

    update server and client: basic function for sending a file in server and client Missing: Exception handling requesting file from server

--- ex2/client.py
+++ ex2/client.py
@@ -1,17 +1,59 @@
-class client:
-    cache_dir = "/home/leon/Schreibtisch/dis_sys/ex2/cache"
-    cached_files = []
-    server_address = "127.0.0.1"
-    port = 5000
+import json
+from pathlib import Path
 
-    def send_file(self):
-        pass
+import requests
 
-    def receive_file(self):
+
+class SimpleClient:
+    def __init__(self, cache_dir, server_address, port, commands):
+        self.cache_dir = cache_dir
+        self.server_address = server_address
+        self.port = port
+        self.cached_files = []
+        self.commands = commands
+
+    def send_file(self, file_name):
+        file = Path(f"{self.cache_dir}/{file_name}").read_text()
+        file_name, extension = file_name.split(".")
+        object_dict = {"filename": file_name, "object": file, "extension": "txt"}
+        object_json = json.dumps(object_dict)
+        respond = requests.post(
+            f"http://{self.server_address}:{self.port}/upload", json=object_json
+        )
+        print(respond)
+
+    def receive_file(self, file_name):
         pass
 
     def get_dir_tree(self):
         pass
 
     def run_client(self):
-        pass
+        while True:
+            request = input()
+            input_cmd = request.split()
+            if (
+                input_cmd[0] not in self.commands
+                or len(input_cmd) < 1
+                or len(input_cmd) > 2
+            ):
+                print("Unknown command! available commands: <send>, <receive>")
+            elif len(input_cmd) == 1:
+                return self.get_dir_tree()
+            elif input_cmd[0] == "send":
+                return self.send_file(input_cmd[1])
+            elif input_cmd[0] == "open":
+                return self.receive_file(input_cmd[1])
+            else:
+                break
+        print("Shutting down client!")
+
+
+cl = SimpleClient(
+    cache_dir="/home/leon/Schreibtisch/dis_sys/ex2/client_storage",
+    commands=["open", "send", "ll"],
+    server_address="127.0.0.1",
+    port=5000,
+)
+
+cl.send_file("test2.txt")
--- ex2/server.py
+++ ex2/server.py
@@ -2,7 +2,7 @@ import json
 
 from flask import Flask, request, send_from_directory, jsonify
 
-STORAGE_PATH = "/home/leon/Schreibtisch/dis_sys/ex2/storage"
+STORAGE_PATH = "/home/leon/Schreibtisch/dis_sys/ex2/server_storage"
 filename = "test.txt"
 directories = {"storage": "test.txt"}
 app = Flask(__name__)
@@ -17,19 +17,20 @@ def hello_world():
 def upload_file():
     if request.method == "POST":
         if request.is_json:
-            request_object = request.get_json()
-            file_path = request_object["file_path"]
+            request_object = json.loads(request.get_json())
+            print(request_object)
+            print(type(request_object))
             file_name = request_object["filename"]
             file = request_object["object"]
-            f = request.files["test"]
-            with open(f"{file_path}/{file_name}.json", "w") as f:
+            with open(f"{STORAGE_PATH}/{file_name}.json", "w") as f:
                 json.dump(file, f)
-            directories[f"{file_path}/{file_name}"] = file_name
+            directories[f"{file_name}"] = f"{STORAGE_PATH}/{file_name}.json"
+            return jsonify("success")
 
 
 @app.route("/<filename>", methods=["GET"])
 def send_file(filename):
-    return send_from_directory("storage", filename, as_attachment=True)
+    return send_from_directory("storage", directories[filename], as_attachment=True)
 
 
 @app.get("/directories")
@@ -38,4 +39,4 @@ def get_directories():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
