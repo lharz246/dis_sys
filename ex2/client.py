15b49d591b71c08930d3372666be8611175391ba
lharz <lharz@techfak.uni-bielefeld.de>
2024-09-29 18:51:43 +0200

    server: - file_names saved as json - completely useless data gets saved - very basic persistent mechanic added client: - allows shell commands - less noticeable network responses

--- ex2/client.py
+++ ex2/client.py
@@ -1,86 +1,112 @@
+import hashlib
+import json
 import os
 from pathlib import Path
-import json
+
 import requests
-import sys
+
+STORAGE_PATH = f"{Path.cwd()}/client_storage"
+
 
 class SimpleClient:
-    def __init__(self, cache_dir, server_address, port, commands):
+    def __init__(self, cache_dir, server_address, port):
         self.cache_dir = os.path.join(os.path.dirname(__file__), cache_dir)
         self.server_address = server_address
         self.port = port
-        self.cached_files = []
-        self.commands = commands
+        self.cached_files = [
+            f for f in Path(f"{Path.cwd()}/client_storage").iterdir() if f.is_file()
+        ]
 
     def send_file(self, file_name):
-      
-        file_path = Path(f"{self.cache_dir}/{file_name}")
-        if not file_path.exists():
+        path = Path(f"{self.cache_dir}/{file_name}")
+        if not path.is_file():
             print(f"Error: {file_name} does not exist in the client storage.")
             return
-
-  
         try:
-            file_content = file_path.read_text(encoding="utf-8")
+            file = path.read_text()
         except Exception as e:
             print(f"Error reading file {file_name}: {e}")
             return
-
-        base_file_name, _ = os.path.splitext(file_name)
-
-       
-        file_payload = {
-            "filename": base_file_name,   
-            "object": file_content        
+        hash_func = hashlib.sha256()
+        hash_func.update(bytes(f"{Path.cwd()}/{file_name}", encoding="utf-8"))
+        file_hash = hash_func.hexdigest()
+        object_dict = {
+            "filename": file_name,
+            "object": file,
+            "extension": "txt",
+            "path": str(Path.cwd()),
+            "hash": file_hash,
         }
-
-        print("Sending payload:", file_payload)  
+        object_json = json.dumps(object_dict)
         response = requests.post(
-            f"http://{self.server_address}:{self.port}/upload", json=file_payload
+            f"http://{self.server_address}:{self.port}/upload", json=object_json
         )
-
         if response.status_code == 200:
             print(f"File '{file_name}' successfully uploaded.")
         else:
             print(f"Error uploading file: {response.text}")
 
-    def receive_file(self, file_name):
+    def receive_file(self, cmd, file_name):
         try:
             local_file_path = Path(f"{self.cache_dir}/{file_name}")
-            if local_file_path.exists():
-                print(f"{file_name} exists locally.")
-                return local_file_path.read_text()
-
-            response = requests.get(f"http://{self.server_address}:{self.port}/{file_name}")
+            if Path(f"{self.cache_dir}/{file_name}").is_file():
+                return os.system(f"{cmd} {self.cache_dir}/{file_name}")
+            hash_func = hashlib.sha256()
+            hash_func.update(bytes(f"{Path.cwd()}/{file_name}", encoding="utf-8"))
+            file_hash = hash_func.hexdigest()
+            response = requests.get(
+                f"http://{self.server_address}:{self.port}/{file_hash}"
+            )
+            print(response.text)
             if response.status_code == 200:
-                file_content = response.text
+                file_content = json.loads(response.text)["object"]
                 with local_file_path.open("w") as f:
                     f.write(file_content)
-                print(f"{file_name} downloaded and saved locally.")
-                return file_content
+                return os.system(f"{cmd} {self.cache_dir}/{file_name}")
             else:
-                print(f"Error: unable to download {file_name} from server. Status code: {response.status_code}")
+                print(
+                    f"Error: unable to download {file_name} from server. Status code: {response.status_code}"
+                )
         except Exception as e:
             print(f"Error while downloading file: {e}")
 
+    def run(self):
+        while True:
+            print(">:", end="")
+            cmd = input()
+            cmd = cmd.split(" ")
+            if cmd[0] == "":
+                pass
+            elif cmd[0] == "send":
+                self.send_file(cmd[1])
+            elif cmd[0] == "exit":
+                print("Shutting down client!")
+                break
+            else:
+                print(f"{cmd[0]} {cmd[1]}")
+                self.receive_file(cmd[0], cmd[1])
+
+
 if __name__ == "__main__":
+    if not os.path.exists(STORAGE_PATH):
+        os.makedirs(STORAGE_PATH)
     client = SimpleClient(
         cache_dir="client_storage",
         server_address="127.0.0.1",
         port=5000,
-        commands=["upload", "download"]
     )
 
-    if len(sys.argv) < 3:
-        print("Usage: python client.py <upload/download> <filename>")
-        sys.exit(1)
-
-    command = sys.argv[1]
-    file_name = sys.argv[2]
-
-    if command == "upload":
-        client.send_file(file_name)
-    elif command == "download":
-        client.receive_file(file_name)
-    else:
-        print("Unknown command! Use 'upload' or 'download'.")
+    client.run()
+    # if len(sys.argv) < 3:
+    #     print("Usage: python client.py <upload/download> <filename>")
+    #     sys.exit(1)
+    #
+    # command = sys.argv[1]
+    # file_name = sys.argv[2]
+    #
+    # if command == "upload":
+    #     client.send_file(file_name)
+    # elif command == "download":
+    #     client.receive_file(file_name)
+    # else:
+    #     print("Unknown command! Use 'upload' or 'download'.")
--- ex2/server.py
+++ ex2/server.py
@@ -1,54 +1,78 @@
-import os
 import json
+from pathlib import Path, PurePath
 from flask import Flask, request, send_from_directory, jsonify, abort
+import os
 
-STORAGE_PATH = os.path.join(os.path.dirname(__file__), "server_storage")
-uploaded_files = {}
+STORAGE_PATH = f"{Path.cwd()}/server_storage"
+if Path(f"{STORAGE_PATH}/file_index/index.json").is_file():
+    with open(f"{STORAGE_PATH}/file_index/index.json", "r") as f:
+        uploaded_files = json.loads(f.read())
+else:
+    uploaded_files = {}
 app = Flask(__name__)
 
+print(uploaded_files)
+
+
 @app.route("/")
-def home():
-    return "<p>Hello, World!</p>"
+def starting():
+    greeting = (
+        "Hello! This is our small and simple file server. "
+        "It allows the user to save files remotely and "
+        "enables the user to work with these files as they "
+        "were locally saved, therefore ensuring network transparency"
+    )
+    return jsonify(greeting)
+
 
 @app.route("/upload", methods=["POST"])
 def upload_file():
     if request.is_json:
-        data = request.get_json()
-        print("Received payload:", data) 
-
-        file_name = data.get("filename")
-        file_content = data.get("object")
-
-     
+        request_object = json.loads(request.get_json())
+        print(f"received:{request_object}")
+        print(request_object)
+        print(type(request_object))
+        file_name = request_object.get("filename")
         if not file_name:
             abort(400, description="Filename is missing.")
-
-        file_path = os.path.join(STORAGE_PATH, f"{file_name}.json")
-        with open(file_path, "w") as file:
-            json.dump(file_content, file)  
-
-        uploaded_files[file_name] = file_path
-        return jsonify({"status": "success", "file": file_name}), 200
+        path = request_object.get("path")
+        file_hash = request_object.get("hash")
+        print(file_hash)
+        with open(f"{STORAGE_PATH}/{file_hash}.json", "w") as f:
+            json.dump(request_object, f)
+        uploaded_files[f"{file_hash}"] = {
+            "file": f"{STORAGE_PATH}/{file_name}.json",
+            "path": path,
+        }
+        with open(f"{STORAGE_PATH}/file_index/index.json", "w") as f:
+            json.dump(uploaded_files, f)
+            print(uploaded_files)
+            return jsonify({"status": "success", "file": file_name}), 200
     return jsonify({"error": "This endpoint only supports POST with JSON data."}), 400
+
 
 @app.route("/<filename>", methods=["GET"])
 def send_file(filename):
-    file_path = os.path.join(STORAGE_PATH, filename)
-
-    if os.path.exists(file_path):
+    file_path = PurePath(STORAGE_PATH, f"{filename}.json")
+    print(file_path)
+    if Path(file_path).is_file():
+        print("HiEr1")
         if filename not in uploaded_files:
             uploaded_files[filename] = file_path
-
-        return send_from_directory(STORAGE_PATH, filename, as_attachment=True)
+        return send_from_directory(STORAGE_PATH, f"{filename}.json", as_attachment=True)
     else:
         abort(404, description=f"File '{filename}' not found.")
+
 
 @app.get("/directories")
 def list_uploaded_files():
     return jsonify(uploaded_files)
 
+
 if __name__ == "__main__":
     if not os.path.exists(STORAGE_PATH):
         os.makedirs(STORAGE_PATH)
-    
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    if not os.path.exists(f"{STORAGE_PATH}/file_index"):
+        os.makedirs(f"{STORAGE_PATH}/file_index")
+
+    app.run(host="127.0.0.1", port=5000, debug=False)
