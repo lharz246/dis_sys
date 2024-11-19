d773045ba9c9d25627752016df72d0748ddd03c9
lharz <lharz@techfak.uni-bielefeld.de>
2024-11-19 19:39:45 +0100

    exercise 8 server and client for using openweathermap api

--- ex8(rest)/wether_client.py
+++ ex8(rest)/wether_client.py
@@ -2,25 +2,47 @@ import hashlib
 import json
 import sys
 from pathlib import Path
+from datetime import datetime
+import colorama
 import requests
 import hashlib
 import json
 import os
 from pathlib import Path
-
+from colorama import init as colorama_init
+from colorama import Fore
+from colorama import Style
 import requests
+
+colorama.init()
 
 
 def print_weather_info(weather_info):
+    degree_sign = "\N{DEGREE SIGN}"
+    hours_forecasted = weather_info["time_frame"]
+    forecast_details = weather_info["forecast_details"]
+    time_stamp = forecast_details["time"]
+    timezone = forecast_details["timezone"]
     current_condition = weather_info["current_condition"]
-    print(current_condition)
-    print("#" * 20)
-    print(weather_info["forecast"])
+    location = weather_info["location"]
+    forecast = weather_info["forecast"]
+    print(
+        f"#########{Fore.GREEN}CURRENT WEATHER{Style.RESET_ALL}#########\n{location['name']}, {location['state']}, {location['country']}, {time_stamp}\n"
+        f"currently: {Fore.MAGENTA}{current_condition['weather']}{Style.RESET_ALL} temperature: {Fore.GREEN}{current_condition['temp']}{degree_sign}C{Style.RESET_ALL}\n"
+        f"feels_like: {Fore.YELLOW}{current_condition['feel_temp']}{Style.RESET_ALL} max_temp: {Fore.RED}{current_condition['max_temp']}{degree_sign}C{Style.RESET_ALL}\n"
+        f"humidity: {Fore.BLUE}{current_condition['humidity']}{Style.RESET_ALL}  min_temp: {Fore.CYAN}{current_condition['temp']}{degree_sign}C{Style.RESET_ALL}\n"
+    )
+    for k, v in forecast.items():
+        print(
+            f"{location['name']}, {location['state']}, {location['country']}, {k}\n"
+            f"Temperature: {Fore.MAGENTA}{v['weather']}{Style.RESET_ALL} temperature: {Fore.GREEN}{v['temp']}{degree_sign}C{Style.RESET_ALL}\n"
+            f"feels_like: {Fore.YELLOW}{v['feel_temp']}{Style.RESET_ALL} max_temp: {Fore.RED}{v['max_temp']}{degree_sign}C{Style.RESET_ALL}\n"
+            f"humidity: {Fore.BLUE}{v['humidity']}{Style.RESET_ALL}  min_temp: {Fore.CYAN}{v['temp']}{degree_sign}C{Style.RESET_ALL}\n"
+        )
 
 
 class SimpleClient:
-    def __init__(self, cache_dir, server_address, port):
-        self.cache_dir = os.path.join(os.path.dirname(__file__), cache_dir)
+    def __init__(self, server_address, port):
         self.server_address = server_address
         self.port = port
 
@@ -30,7 +52,7 @@ class SimpleClient:
         weather_request["type"] = "weather_request"
         weather_request["location"] = city
         weather_request["time_frame"] = time_frame
-        weather_request["day_format"] = 0 if hourly else 1
+        weather_request["day_format"] = hourly
         weather_request = json.dumps(weather_request)
         response = requests.post(
             f"http://{self.server_address}:{self.port}/forecast", json=weather_request
@@ -44,7 +66,6 @@ class SimpleClient:
 
 if __name__ == "__main__":
     client = SimpleClient(
-        cache_dir="client_storage",
         server_address="127.0.0.1",
         port=5000,
     )
@@ -56,9 +77,7 @@ if __name__ == "__main__":
         sys.exit(1)
     else:
         city = sys.argv[1]
-        time_frame = (
-            int(sys.argv[2]) if sys.argv[3] == "hourly" else int(sys.argv[2]) * 24
-        )
+        time_frame = int(sys.argv[2])
         client.get_weather_info(
             city, time_frame, True if sys.argv[3] == "hourly" else False
         )
--- ex8(rest)/wether_server.py
+++ ex8(rest)/wether_server.py
@@ -1,9 +1,9 @@
+import datetime
 import json
-from pathlib import Path, PurePath
-
+import math
+from datetime import datetime, timedelta
 import requests
-from flask import Flask, request, send_from_directory, jsonify, abort
-import os
+from flask import Flask, request, jsonify, abort
 from dotenv import dotenv_values
 
 app = Flask(__name__)
@@ -25,9 +25,8 @@ def forecast():
     elif data["time_frame"] == "empty":
         abort(400, description="Please provide time frame!")
     else:
-        hours_forecasted = min(120, int(data["time_frame"]))
-        hours_forecasted = hours_forecasted // 3
-        hour_format = True if data["day_format"] == "hourly" else False
+
+        hour_format = data["day_format"]
         geo_loc = requests.get(
             f"{geocoding_url}{data['location']}&limit=1&appid={API_KEY}"
         ).json()
@@ -44,13 +43,24 @@ def forecast():
         if current_weather["cod"] != 200:
             abort(400, description="Server Problem3 :S")
         info_responds = build_responds(
-            weather_forecast, current_weather, hours_forecasted, hour_format
+            weather_forecast,
+            current_weather,
+            geo_loc[0],
+            data["time_frame"],
+            hour_format,
         )
         return jsonify({"staus": "ok", "weather": info_responds}, 200)
 
 
-def build_responds(weather_forecast, current_weather, hours_forecasted, hour_format):
-    degree_sign = "\N{DEGREE SIGN}"
+def build_responds(
+    weather_forecast,
+    current_weather,
+    geo_data,
+    time_frame,
+    hour_format,
+):
+
+    request_time = datetime.fromtimestamp(current_weather["dt"])
     current_condition = {
         "weather": current_weather["weather"][0]["description"],
         "temp": current_weather["main"]["temp"],
@@ -64,16 +74,85 @@ def build_responds(weather_forecast, current_weather, hours_forecasted, hour_for
         "detail_lvl": "hourly" if hour_format else "daily",
         "station_name": current_weather["name"],
         "system": current_weather["sys"],
-        "time": current_weather["dt"],
+        "time": request_time.strftime("%d-%m-%Y"),
+        "timezone": current_weather["timezone"],
     }
+    forecast_data = {}
+    if hour_format:
+        offset = 1 if request_time.hour % 3 == 0 else 0
+        data_points = math.ceil(time_frame / 3)
+        weather_data = weather_forecast["list"][offset : data_points + offset]
+        idx = 0
+        for i in range(time_frame):
+            forecast_data[
+                f"{(request_time + timedelta(hours=i + offset)).strftime('%d-%m-%Y %H:%M:%S')}"
+            ] = {
+                "weather": weather_data[idx]["weather"][0]["description"],
+                "temp": weather_data[idx]["main"]["temp"],
+                "max_temp": weather_data[idx]["main"]["temp_max"],
+                "min_temp": weather_data[idx]["main"]["temp_min"],
+                "feel_temp": weather_data[idx]["main"]["feels_like"],
+                "humidity": weather_data[idx]["main"]["humidity"],
+                # "wind": weather_data[idx]["wind"],
+            }
+            if math.ceil(i / 3) > 1:
+                idx += 1
+    else:
+        starting_day = (request_time + timedelta(days=1)).date()
+        delta = (
+            datetime.combine(starting_day, datetime.min.time()) - request_time
+        ).seconds / 10800
+        offset = math.floor(delta) if request_time.hour % 3 == 0 else math.ceil(delta)
+        data_points = time_frame * 8
+        weather_data = weather_forecast["list"][offset : data_points + offset]
+        counter = 0
+        weather_list = []
+        temp_list = []
+        max_temp = 0
+        min_temp = 100
+        feel_temp_list = []
+        humiditiy_list = []
+        key = starting_day
+        for data in weather_data:
+            if data["weather"][0]["description"] not in weather_list:
+                weather_list.append(data["weather"][0]["description"])
+            temp_list.append(data["main"]["temp"])
+            feel_temp_list.append(data["main"]["feels_like"])
+            humiditiy_list.append(data["main"]["humidity"])
+            if data["main"]["temp_max"] > max_temp:
+                max_temp = data["main"]["temp_max"]
+            if data["main"]["temp_min"] < min_temp:
+                min_temp = data["main"]["temp_min"]
+            counter += 1
+            if counter % 8 == 0 and counter:
+                forecast_data[f"{key}"] = {
+                    "weather": weather_list,
+                    "temp": sum(temp_list) / float(len(temp_list)),
+                    "max_temp": max_temp,
+                    "min_temp": min_temp,
+                    "feel_temp": sum(feel_temp_list) / float(len(feel_temp_list)),
+                    "humidity": sum(humiditiy_list) / float(len(humiditiy_list)),
+                    # "wind": [],
+                }
+                weather_list = []
+                temp_list = []
+                max_temp = 0
+                min_temp = 100
+                feel_temp_list = []
+                humiditiy_list = []
+                key = datetime.fromtimestamp(data["dt"]).date()
 
     response = {
         "type": "weather_info",
-        "location": weather_forecast["city"],
+        "location": {
+            "name": geo_data["name"],
+            "state": geo_data["state"],
+            "country": geo_data["country"],
+        },
         "forecast_details": forecast_details,
-        "time_frame": hours_forecasted,
+        "time_frame": time_frame,
         "current_condition": current_condition,
-        "forecast": weather_forecast,
+        "forecast": forecast_data,
     }
     return response
 
